65171a4a000666fe7ec523068100b803b3979752
GolubNastia <av_golub@mail.ru>
2024-06-07 11:17:35 +0300

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,21 @@
+a = 'Машина'
+print( len(a) ) # длинна слова
+first = 10
+second = 7
+summa = first+second
+diff = first-second
+print (summa)
+print (diff) # сумма и разности
+first = 10
+second = 7
+third = 4
+mean = (first+second+third)/3
+print(mean) #среднее арифметическое
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(second_string,", ",first_string) # Простые строчки
+a = 10
+b = 7
+c = 4
+f = (((a*b) + (a*c))**3)/2
+print(f) #Сложная формула
